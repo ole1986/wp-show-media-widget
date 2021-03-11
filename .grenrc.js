37f8d73addd6d73e247c349a107ab117559150de
ole1986 <ole.koeckemann@gmail.com>
2021-03-11 10:02:27 +0100

    Automate release notes using gren

---
+++ .grenrc.js
@@ -0,0 +1,24 @@
+function getAuthor(placeholders) {
+    if (placeholders.author === 'ole1986') {
+        // skip owner
+        return '';
+    }
+
+    if (placeholders.author === null) {
+        // skip when no author could be found
+        return '';
+    }
+
+    return `- @${placeholders.author}`;
+}
+
+function parseCommitLine(placeholders) {
+    return `- ${placeholders.message} ${getAuthor(placeholders)}`
+}
+
+module.exports = {
+    dataSource: "commits",
+    "template": {
+        commit: parseCommitLine
+    }
+}
